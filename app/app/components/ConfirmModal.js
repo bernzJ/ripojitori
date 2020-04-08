8829247e472b00f7b1d56e46ca3b5ddabca79b3f
Ben J <ben-0_0-@hotmail.com>
2020-04-08 05:03:52 -0400

    Styling fix: styles on the confirm modal.

--- app/app/components/ConfirmModal.js
+++ app/app/components/ConfirmModal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Button, Modal } from 'react-bootstrap';
 
@@ -9,20 +10,16 @@ const ModalComponent = ({ show, hide, title, message, handleResponse }) => {
   };
 
   return (
-    <Modal show={show} onHide={() => hide(false)}>
+    <ModalWrapper show={show} onHide={() => hide(false)} centered>
       <Modal.Header closeButton>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>{message}</Modal.Body>
       <Modal.Footer>
-        <Button onClick={() => handleClick(false)} variant="success">
-          Cancel
-        </Button>
-        <Button onClick={() => handleClick(true)} variant="danger">
-          Confirm
-        </Button>
+        <CancelButton onClick={() => handleClick(false)}>Cancel</CancelButton>
+        <SaveButton onClick={() => handleClick(true)}>Confirm</SaveButton>
       </Modal.Footer>
-    </Modal>
+    </ModalWrapper>
   );
 };
 export default ModalComponent;
@@ -34,3 +31,54 @@ ModalComponent.propTypes = {
   message: PropTypes.string.isRequired,
   handleResponse: PropTypes.func.isRequired
 };
+
+const ModalWrapper = styled(Modal)`
+  &&& .modal-content,
+  &&& .modal-header,
+  &&& .modal-footer {
+    border: none;
+    border-radius: 0;
+    color: #00355c;
+    background-clip: none;
+  }
+  &&& .modal-header {
+    background-color: #4898cf;
+    color: #fff;
+  }
+  &&& .modal-content input {
+    border-radius: 0;
+  }
+  &&& .modal-content .dropdown .dropdown-toggle {
+    border: 1px solid #ced4da;
+  }
+`;
+const SaveButton = styled(Button)`
+  &&& {
+    text-transform: uppercase;
+    color: #fff;
+    background-color: #4898cf;
+    transition: background-color 0.2s linear;
+    border: none;
+  }
+  &&&:hover,
+  &&&:focus {
+    outline: none;
+    box-shadow: none;
+    background-color: #5abcff;
+  }
+`;
+const CancelButton = styled(Button)`
+  &&& {
+    text-transform: uppercase;
+    color: #fff;
+    background-color: #969696;
+    transition: background-color 0.2s linear;
+    border: none;
+  }
+  &&&:hover,
+  &&&:focus {
+    outline: none;
+    box-shadow: none;
+    background-color: #c5c5c5;
+  }
+`;
